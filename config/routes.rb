fd8ccd55aaeafbedd6cd820d5b54c57743e9143c
Amie Wilt <amie.wilt@gmail.com>
2015-06-08 16:25:55 -0400

    generated home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
+  # resources :comments
+  # resources :posts
+
   get 'comments/index'
 
   get 'posts/index'
+
+  root 'posts#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
