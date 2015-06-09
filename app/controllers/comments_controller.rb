3f039e66324fb6597ba55b7e2298a7071b6ee6a0
Amie Wilt <amie.wilt@gmail.com>
2015-06-09 16:38:29 -0400

    added user reference to post, got it so comments are printing out in show post, created user relationship

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -43,6 +43,6 @@ class CommentsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def comment_params
-    params.require(:comment).permit(:post_id, :title, :author_name, :author_email, :body)
+    params.require(:comment).permit(:title, :author_name, :author_email, :body)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
 class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
+  belongs_to :user
 
   validates :title, presence: true
-  validates :author, presence: true
+  # validates :author, presence: true
   validates :body, presence: true
 end
