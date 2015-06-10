4fc2e2b8f464677095f4f01cc39bfab491f08948
Amie Wilt <amie.wilt@gmail.com>
2015-06-10 09:52:11 -0400

    fixed unrecognized current user method, helper method :current_user was commented out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
-  # helper_method :@current_user
+  helper_method :current_user
 
   def require_authenticated_user
     unless current_user
