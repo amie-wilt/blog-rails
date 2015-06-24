4acc30b5e0915689d89af7c899b9102acbb6da5d
Amie Wilt <amie.wilt@gmail.com>
2015-06-24 09:08:00 -0400

    added stripe gem and api keys

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ gem 'mini_magick'
 # file upload solution
 gem 'carrierwave'
 
+gem 'stripe'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,11 +1,12 @@
 # CarrierWave.configure do |config|
 #   config.fog_credentials = {
-#       provider:              'AWS',
-#       aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+#       provider: 'AWS',
+#       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
 #       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-#       region:                'us-east-1'
+#       region: 'us-east-1'
 #   }
-#   config.fog_directory  = Rails.env.development? ? 'amie-rails-blog-dev' : 'amie-rails-blog'
-#   config.fog_public     = false
-#   config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }
+#
+#   config.fog_directory  = ENV['AWS_BUCKET_NAME']
+#   config.fog_public = false
+#   config.fog_attributes = {'Cache-Control' => "max-age=#{365.day.to_i}"}
 # end
